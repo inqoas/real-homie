0460d3bffe1d8e27009463a0e56cb9cda0086102
pong <inqoas@gmail.com>
2023-07-21 09:26:53 +0800

    目錄結構

---
+++ real-homie/src/main/java/tw/idv/tibame/tha102/web/userinfo/service/UserInfoService.java
@@ -0,0 +1,12 @@
+package tw.idv.tibame.tha102.web.userinfo.service;
+
+import java.util.List;
+
+import tw.idv.tibame.tha102.userinfo.vo.UserInfo;
+
+public interface UserInfoService {
+	public List<UserInfo> findAll();
+	public UserInfo updateUser(UserInfo user);
+	public UserInfo newUser(UserInfo user);
+	public List<UserInfo> findUserByName(String userName);
+}
---
+++ real-homie/src/main/java/tw/idv/tibame/tha102/web/userinfo/service/impl/MemberServiceImpl.java
@@ -0,0 +1,27 @@
+package tw.idv.tibame.tha102.web.userinfo.service.impl;
+
+import java.lang.reflect.Member;
+
+import tw.idv.tibame.tha102.userinfo.service.MemberService;
+
+public class MemberServiceImpl implements MemberService{
+
+	@Override
+	public Member register(Member member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Member login(Member member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Member edit(Member member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
